516b26e6dfa2904f988c1bcee788f7920540eb82
Poteko Sakamoto <poteko@poteko.moe>
2020-05-10 23:03:08 +0800

    minor improvement

--- AzureDDNS/Settings/DnsZone.cs
+++ AzureDDNS/Settings/DnsZone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace AzureDDNS.Settings
 {
     public class DnsZone
     {
+        [Required]
         public string ResourceGroup { get; set; }
 
+        [Required]
         public string ZoneName { get; set; }
     }
 }
